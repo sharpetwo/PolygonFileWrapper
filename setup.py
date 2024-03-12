aa10a8c95b3cbba9372b1a8c2dc1a7763020d0f9
Goran Peretin <goran.peretin@gmail.com>
2024-03-12 10:38:40 +0100

    Correct module in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='polygon_wrapper',
     version='0.1',
     description='Polygon.io flat file downloader',
+    py_modules=["polygon_wrapper"],
     install_requires=[
         'boto3'
         'polars',
