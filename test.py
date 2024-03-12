4cbc311c1a5311fca6ab309db55bd68dad056e82
sharpetwo <alex@sharpetwo.com>
2024-03-12 14:00:57 -0600

    Modif test.py to add the failing test

--- test.py
+++ test.py
@@ -7,13 +7,17 @@ def test_get_list_objects():
     objects = wrapper.get_list_objects(verbose=True)
     assert len(objects) > 30
 
-def test_download_from_list_objects():
-    """ This test assumes that at least a 1000 rows will be returned"""
+def test_download_from_list_objects(year,month,partition):
+    """ This test will download the files and save them in a tmp directory"""
     wrapper = PolygonFileWrapper()
-    df = wrapper.download_from_list_objects(partition=False,save_disk=False)
-    assert len(df) > 1_000
+    df = wrapper.download_from_list_objects(year,month,partition)
+    assert len(df) > 10_000    
 
 def test_get_options_trades():
     wrapper = PolygonFileWrapper()
     df = wrapper.download_trades_parquet(dt.date(2024, 3, 8), dt.date(2024, 3, 10))
     assert len(df) > 1_000_000
+
+if __name__ == '__main__':
+    test_get_list_objects() # Pass
+    test_download_from_list_objects(year=2024,month=2,partition=True) # Break because of clean_option_df when Wrapper is for STOCKS MINUTES
