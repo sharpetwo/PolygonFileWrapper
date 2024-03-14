89bb69eb8871b54a684158219334a0f859e03922
sharpetwo <alex@sharpetwo.com>
2024-03-14 13:56:01 -0600

    Forgot the tests

--- test.py
+++ test.py
@@ -1,22 +1,35 @@
 import datetime as dt
+import os
 from polygon_wrapper import PolygonFileWrapper
 
 
-def test_get_list_objects():
-    """ This test assumes that at least a full month of data will be returned """
-    wrapper = PolygonFileWrapper()
-    objects = wrapper.get_list_objects(verbose=True)
-    assert len(objects) > 30
 
-
-def test_download_from_list_objects():
+def test_get_list_objects(year,month,partition):
     """ This test will download the files and save them in a tmp directory"""
     wrapper = PolygonFileWrapper()
-    df = wrapper.download_from_list_objects(year=2024, month=2, partition=True)
-    assert len(df) > 10_000
+    list_objects = wrapper.get_list_objects(year,month,partition)
+    assert len(list_objects) > 10
 
-
-def test_get_options_trades():
+def test_download_history_on_disk(start_date, end_date):
+    """This test will download a full history between a starting date and an end date
+    and then asserts that at least one .parquet file is stored in wrapper.datadir."""
     wrapper = PolygonFileWrapper()
-    df = wrapper.download_trades_parquet(dt.date(2024, 3, 8), dt.date(2024, 3, 10))
+    wrapper.download_history_on_disk(start_date, end_date)
+
+    # Count .parquet files in the directory
+    parquet_files = [f for f in os.listdir(wrapper.datadir) if f.endswith(".parquet")]
+    number_of_parquet_files = len(parquet_files)
+
+    assert number_of_parquet_files > 0
+
+def test_download_single_date(date):
+    """ This test will download a full history for a given date"""
+    wrapper = PolygonFileWrapper()
+    df = wrapper.download_single_date(date)
+    assert len(df) > 100_000
+
+def test_download_history_in_memory(start_date, end_date=None):
+    """ This test will download the history between the two dates in memory"""
+    wrapper = PolygonFileWrapper()
+    df = wrapper.download_history_in_memory(start_date, end_date)
     assert len(df) > 1_000_000
